956d4d9b448f3d8d886c40b031b7cc4cb39d3b7f
Alex Horning <68720206+alexandertbh@users.noreply.github.com>
2023-04-12 11:42:09 -0700

    added css fixes for media sizes

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 var modalSelection = document.getElementById("dropdown1");
 
-// This is the event listener for the modal 
+// This is the event listener for the modal
 document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".modal");
-    var instances = M.Modal.init(elems);
-  });
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems);
+});
 
 // This is the event listener for the dropdown menu
-  document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll("#dropdown-trigger");
-    var instances = M.Dropdown.init(elems);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll("#dropdown-trigger");
+  var instances = M.Dropdown.init(elems);
+});
 
-var beerButton = document.querySelector('.dropdown-trigger-1');
-var beerPreferenceItem = document.querySelectorAll('#dropdown1');
-beerPreferenceItem.forEach(item => {item.addEventListener('click', event=>{
-    beerButton.textContent = event.target.textContent;});
-    
+var beerButton = document.querySelector(".dropdown-trigger-1");
+var beerPreferenceItem = document.querySelectorAll("#dropdown1");
+beerPreferenceItem.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    beerButton.textContent = event.target.textContent;
+  });
 });
 
 function storeValue(event) {
@@ -24,7 +25,6 @@ function storeValue(event) {
   var modalValue = event.target.id;
   localStorage.setItem("modalValue", modalValue);
   return modalValue;
- }
- 
- 
- modalSelection.addEventListener("click", storeValue);
+}
+
+modalSelection.addEventListener("click", storeValue);
